01e98c3790a6fc8bb4604c96f47961e054ed4ef5
Segun Aruleba <segsywealthprosperity@gmail.com>
2026-04-28 21:29:04 +0100

    update constants.js

--- src/constants.js
+++ src/constants.js
@@ -1,7 +1,7 @@
 //BRAND
 export const name = 'AiCourse';
 export const company = 'TechSolution Devs';
-export const websiteURL = 'http://localhost:3000';
+export const websiteURL = 'https://ai-course-generator-gold.vercel.app';
 export const serverURL = 'http://localhost:5000';
 export const logo = 'https://firebasestorage.googleapis.com/v0/b/india-bd34b.appspot.com/o/artificial-intelligence.png?alt=media&token=2dc0da1c-12d0-4dd2-b908-cb8e57fcf3f0';
 export const razorpayEnabled = true;
