cfc7703d68f43737240a72ceb1457fda54043df7
Segun Aruleba <segsywealthprosperity@gmail.com>
2026-04-28 01:31:45 +0100

    add constrants.js

--- src/constants.js
+++ src/constants.js
@@ -2,7 +2,7 @@
 export const name = 'AiCourse';
 export const company = 'TechSolution Devs';
 export const websiteURL = 'https://ai-course-generator-gold.vercel.app';
-export const serverURL = 'https://ai-course-generator-gold.vercel.app';
+export const serverURL = 'http://localhost:5000';
 export const logo = 'https://firebasestorage.googleapis.com/v0/b/india-bd34b.appspot.com/o/artificial-intelligence.png?alt=media&token=2dc0da1c-12d0-4dd2-b908-cb8e57fcf3f0';
 export const razorpayEnabled = true;
 export const paypalEnabled = true;
